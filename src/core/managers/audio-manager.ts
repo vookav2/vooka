81d1ed254ae3e2d6d55bdbcf4cecae41aea8161d
mr687 <dapino.dev@gmail.com>
2023-03-13 03:11:34 +0700

    another audio stream factory

--- src/core/managers/audio-manager.ts
+++ src/core/managers/audio-manager.ts
@@ -1,4 +1,5 @@
-import { AudioPlayer, NoSubscriberBehavior, StreamType, createAudioResource } from '@discordjs/voice'
+import { AudioPlayer, NoSubscriberBehavior, createAudioResource } from '@discordjs/voice'
+import { YouTubeStream, stream as playDlStream } from 'play-dl'
 import { create as createYoutubeDL, exec as youtubeDlExec } from 'youtube-dl-exec'
 
 import { Readable } from 'stream'
@@ -21,40 +22,23 @@ export const makeAudioPlayer = () => {
   return audioPlayer
 }
 
-export const createAudioResourceFromStream = <T>(stream: Readable | string, metadata?: T) =>
-  createAudioResource<T>(stream, {
-    inputType: StreamType.WebmOpus,
+export const createAudioResourceFromStream = <T>(stream: Readable | string | YouTubeStream, metadata?: T) => {
+  if (typeof stream === 'string' || stream instanceof Readable) {
+    return createAudioResource<T>(stream, {
+      silencePaddingFrames: 5,
+      inlineVolume: false,
+      metadata,
+    })
+  }
+  return createAudioResource<T>(stream.stream, {
+    inputType: stream.type,
     silencePaddingFrames: 5,
     inlineVolume: false,
     metadata,
   })
-
-export const createAudioWebmUrl = async (id: string, _refererId?: string) => {
-  const logger = makeLogger('CreateAudioWebmUrl')
-  const youtubeDl = createYoutubeDL(YOUTUBE_DL_PATH)
-  const url = `https://www.youtube.com/watch?v=${id}`
-
-  logger.debug('Create audio webm url')
-  logger.debug(url)
-
-  const ytResponse = await youtubeDl(url, {
-    dumpSingleJson: true,
-    noCheckCertificates: true,
-    noWarnings: true,
-    format: 'ba[aext=webm][acodec=opus][asr=48000]/ba[acodec=opus]',
-    addHeader: [
-      'referer:youtube.com',
-      'user-agent:Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-    ],
-  })
-
-  logger.debug('Got response from youtube-dl')
-  logger.debug(ytResponse.url)
-
-  return ytResponse?.url ?? null
 }
 
-export const createAudioStream = async (id: string, _refererId?: string) =>
+export const createAudioStreamYtDl = async (id: string, _refererId?: string) =>
   new Promise<Readable>((resolve, reject) => {
     const logger = makeLogger('CreateAudioReadable')
     const childProcess = youtubeDlExec(
@@ -92,3 +76,38 @@ export const createAudioStream = async (id: string, _refererId?: string) =>
 
     childProcess.once('spawn', onChildProcessSpawn).catch(onChildProcessSpawnError)
   })
+
+export const createAudioWebmUrl = async (id: string, _refererId?: string) => {
+  const logger = makeLogger('CreateAudioWebmUrl')
+  const youtubeDl = createYoutubeDL(YOUTUBE_DL_PATH)
+  const url = `https://www.youtube.com/watch?v=${id}`
+
+  logger.debug('Create audio webm url')
+  logger.debug(url)
+
+  const ytResponse = await youtubeDl(url, {
+    dumpSingleJson: true,
+    noCheckCertificates: true,
+    noWarnings: true,
+    format: 'ba[aext=webm][acodec=opus][asr=48000]/ba[acodec=opus]',
+    addHeader: [
+      'referer:youtube.com',
+      'user-agent:Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+    ],
+  })
+
+  logger.debug('Got response from youtube-dl')
+  logger.debug(ytResponse.url)
+
+  return ytResponse?.url ?? null
+}
+
+export const createAudioStream = async (id: string, _refererId?: string) => {
+  const logger = makeLogger('createAudioStream[PlayDL]')
+  const url = `https://www.youtube.com/watch?v=${id}`
+
+  logger.debug('Create audio stream')
+  return await playDlStream(url, {
+    htmldata: false,
+  })
+}
